8403554b3213e74889e7e9b3a8b4bdf598e73c22
Rupert Adams <RupertNCAdams@aol.com>
2018-09-25 13:24:48 +0100

    step 10

--- directory.rb
+++ directory.rb
@@ -1,7 +1,40 @@
 #first we out the students into an array
+@students = []
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  group_by_cohort
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # This causes the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
 def input_students
-  #create an empty array
-  students = []
   #get the name
   name =
   until name == 'stop' do
@@ -9,7 +42,7 @@ def input_students
   puts "Enter 'stop' to finish student input"
   name = gets.strip
   if name == 'stop'
-    students.count -1
+    @students.count -1
     break
   end
   #while the name is not empty, repeat this code
@@ -26,10 +59,10 @@ def input_students
   age = gets.strip
   #while age is not empty, repeat this code
     #add the student has to the array
-    students << {name: name, cohort: cohort, country: country, age: age}
+    @students << {name: name, cohort: cohort, country: country, age: age}
   end
 
-  students
+  @students
 end
 
 #next, we print them
@@ -38,10 +71,10 @@ def print_header
   puts "-------------".center(40)
 end
 
-def print_m(students)
+def print_students_list
   count = 0
-until count == students.count
-  students.each.with_index(1) do |student, index|
+until count == @students.count
+  @students.each.with_index(1) do |student, index|
      if student[:name][0] == "M" && student[:name].length <= 12
        puts " "
        puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)".center(40)
@@ -52,10 +85,10 @@ until count == students.count
  end
 end
 
-def group_by_cohort(students)
+def group_by_cohort
   group_by_cohort = {}
 
-  students.each do |student|
+  @students.each do |student|
     cohort = student[:cohort]
     name  = student[:name]
 
@@ -75,12 +108,12 @@ def group_by_cohort(students)
   end
 end
 
-def print_footer(names)
+def print_footer
   puts "-------------".center(40)
-  if names.count == 1
-    puts "Overall, we have #{names.count} great student".center(40)
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student".center(40)
   else
-    puts "Overall, we have #{names.count} great students".center(40)
+    puts "Overall, we have #{@students.count} great students".center(40)
   end
 end
 
@@ -90,8 +123,4 @@ def index_of_students(students)
   end
 end
 #finally we print the total number of students
-students = input_students
-print_header
-print_m(students)
-group_by_cohort(students)
-print_footer(students)
+interactive_menu
